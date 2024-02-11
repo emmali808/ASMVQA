a3c899f6c2da896470bb4595b0a89d61af78083b
Your Name <you@example.com>
2024-02-12 02:23:52 +0800

    test14

--- MICCAI21_MMQ/tools/download_data_RAD.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-## <link>
-## This code is written by Binh X. Nguyen
-## Script for downloading preprocessed VQA-RAD data
-mkdir data_RAD
-wget "link"
-unzip data_RAD.zip -d data_RAD
